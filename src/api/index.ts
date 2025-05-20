dfe3020f801cbce2e54a161ac55415398ce77423
张泽全 <hengwujun128@gmail.com>
2025-05-20 14:58:45 +0800

    init project

---
+++ src/api/index.ts
@@ -0,0 +1,9 @@
+/*
+ * @Author: 张泽全 hengwujun128@gmail.com
+ * @Date: 2023-11-20 20:55:53
+ * @LastEditors: 张泽全 hengwujun128@gmail.com
+ * @LastEditTime: 2025-05-20 14:46:49
+ * @Description:
+ * @FilePath: /my-uni-vue3-ts-vite-project/src/api/index.ts
+ */
+export { default as userApi } from './modules/user'
---
+++ src/components/unocss/index.vue
@@ -0,0 +1,25 @@
+<!--
+ * @Author: 张泽全 hengwujun128@gmail.com
+ * @Date: 2023-11-20 20:55:53
+ * @LastEditors: 张泽全 hengwujun128@gmail.com
+ * @LastEditTime: 2025-05-20 14:43:20
+ * @Description: 
+ * @FilePath: /my-uni-vue3-ts-vite-project/src/components/unocss/index.vue
+-->
+<template>
+  <view flex justify-center flex-col items-center my-2>
+    <view
+      w-100
+      h-100
+      m-3
+      animate-iteration-infinite
+      animate-swing
+      bg-cover
+      class="bg-[url(https://img.cdn.sugarat.top/mdImg/MTY2ODA4OTc3MjcyMg==unocss-icon-gray.svg)]"
+    ></view>
+
+    <view class="bg-#3498db/20 text-#3498db rounded" p="x2 y1"
+      >unocss demo</view
+    >
+  </view>
+</template>
